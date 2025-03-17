a7d4752ad7a66d1927a6bc816ac017175b5b8854
Steven <stevenmdmcleod@gmail.com>
2025-03-16 21:17:25 -0400

    changed env file to be ignored by git ignore

--- controller/ticketController.js
+++ controller/ticketController.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const logger = require("../util/logger");
 const jwt = require("jsonwebtoken");
+const ticketService = require('../service/ticketService');
 
 router.get("/", (req, res) => {
     res.status(200).send("This is the root tickets route");
