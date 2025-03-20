31c14ca1fbef3c7020b2198306981042bec54ce8
Steven <stevenmdmcleod@gmail.com>
2025-03-20 01:27:57 -0400

    added ignore pattern for jest tests in package.json and set up imports for ticket.test.js

--- test/ticket.test.js
+++ test/ticket.test.js
@@ -1,3 +1,9 @@
+const ticketService = require("../service/ticketService");
+const dao = require("../repository/ticketDAO");
+const uuid = require("uuid")
+jest.mock("../repository/ticketDAO");
+jest.mock('uuid');
+
 test('filler', () =>{
 
     expect(2+2).toBe(4);
